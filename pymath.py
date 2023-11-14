5df3200ccc5d48bf090e78e768246c9289cd5e9b
Theo Skutar <140082840+MiniSkoot32@users.noreply.github.com>
2023-11-14 07:45:55 -0700

    Created and coded pymath.py

---
+++ pymath.py
@@ -0,0 +1,19 @@
+def add(*args):
+        res = sum(args)
+        return res
+
+def subtract(num1, num2):
+        res = num1 - num2
+        return res
+
+def multiply(num1, num2):
+        res = num1 * num2
+        return res
+
+def divide(num1, num2):
+        res = num1 / num2
+        return res
+
+def floor_divide(num1, num2):
+        res = num1 // num2
+        return res
