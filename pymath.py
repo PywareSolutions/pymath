ea0948e73c50c37da2a2ff91bc00d6e5515629d4
Theo Skutar <140082840+MiniSkoot32@users.noreply.github.com>
2023-11-14 08:13:55 -0700

    Updated pymath.py

--- pymath.py
+++ pymath.py
@@ -17,3 +17,22 @@ def divide(num1, num2):
 def floor_divide(num1, num2):
         res = num1 // num2
         return res
+
+def math(num1, op, num2):
+        if op == "+":
+                res = sum(num1, num2)
+                return res
+        elif op == "-":
+                res = num1 - num2
+                return res
+        elif op == "*" or op.lower == "x":
+                res = num1 * num2
+                return res
+        elif op == "/":
+                res = num1 / num2
+                return res
+        elif op == "//":
+                res = num1 // num2
+                return res
+        else:
+                print("Error. Operator unknown. Try again.")
